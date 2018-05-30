5d49acb63ca150586a008cebbc6e3963c657b2bf
xyyvsxh@gmail.com <xyyvsxh@gmail.com>
2018-05-30 16:37:58 +0800

    init

---
+++ merlin.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.path.append(r"./Lisa/src")
+from lisa import App, route, wserver, redirect
+
+
+@route('/login')
+def login():
+	username = wserver.req.params['username']
+	password = wserver.req.params['password']
+	if username == 'admin' and password == '123':
+		return '{"code":0}'
+	else:
+		return '{"code":1}'
+
+
+def main():
+    app = App()
+    client_path = "file://" + os.getcwd() + "/client/login.html"
+    app.init(client_path, debug=True)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
